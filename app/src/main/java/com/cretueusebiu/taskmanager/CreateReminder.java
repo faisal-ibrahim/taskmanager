7114794a26678ed52e2f80215bfd3ba19d34e45e
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2016-01-11 20:44:54 +0200

    Update reminder.

--- app/src/main/java/com/cretueusebiu/taskmanager/CreateReminder.java
+++ app/src/main/java/com/cretueusebiu/taskmanager/CreateReminder.java
@@ -1,18 +1,23 @@
 package com.cretueusebiu.taskmanager;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.cretueusebiu.taskmanager.alarm.AlarmReceiver;
+import com.cretueusebiu.taskmanager.alarm.ReminderManager;
 import com.cretueusebiu.taskmanager.models.Reminder;
 
 import java.util.Calendar;
 
 public class CreateReminder extends AbstractReminderActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,7 +38,9 @@ public class CreateReminder extends AbstractReminderActivity {
             return false;
         }
 
-        Reminder reminder = Reminder.create(text, calendar, !isTimeVisible());
+        Reminder reminder = Reminder.create(text, calendar, !isTimeVisible(), checkedRepeatItem);
+
+        new ReminderManager(this).set(reminder);
 
         setResult(RemindersActivity.RESULT_OK, new Intent());
 
---
+++ app/src/main/java/com/cretueusebiu/taskmanager/alarm/AlarmReceiver.java
@@ -0,0 +1,88 @@
+package com.cretueusebiu.taskmanager.alarm;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.cretueusebiu.taskmanager.R;
+import com.cretueusebiu.taskmanager.models.Reminder;
+
+import java.util.Calendar;
+
+import static android.media.RingtoneManager.*;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    private static final int NOTIFICATION_ID = 1;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String id = intent.getStringExtra("reminder_id");
+
+        if (id == null) {
+            return;
+        }
+
+        Reminder reminder = Reminder.find(id);
+
+        NotificationCompat.Builder notification =
+                new NotificationCompat.Builder(context)
+                        .setSmallIcon(R.drawable.ic_access_time)
+                        .setContentTitle("Reminder")
+                        .setContentText(reminder.getText());
+
+//        Intent i = new Intent();
+//        i.setAction("DONE");
+//        PendingIntent pi = PendingIntent.getBroadcast(context, 0, i, PendingIntent.FLAG_UPDATE_CURRENT);
+//        notification.addAction(R.drawable.ic_clear, "Done", pi);
+
+        NotificationManager notificationManager = (NotificationManager)
+                context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(NOTIFICATION_ID, notification.build());
+
+        playSound(context);
+
+        Calendar calendar = reminder.getCalendar();
+
+        if (reminder.isAllDay()) {
+            calendar.add(Calendar.HOUR_OF_DAY, 3);
+        } else {
+            switch (reminder.getRepeat()) {
+                case Reminder.REPEAT_MINUTE:
+                    calendar.add(Calendar.MINUTE, 1);
+                    break;
+
+                case Reminder.REPEAT_HOUR:
+                    calendar.add(Calendar.HOUR_OF_DAY, 1);
+                    break;
+
+                case Reminder.REPEAT_DAY:
+                    calendar.add(Calendar.DAY_OF_MONTH, 1);
+                    break;
+            }
+        }
+
+        reminder.setCalendar(calendar);
+        reminder.save();
+    }
+
+    private void playSound(Context context) {
+        try {
+            Uri notification = getDefaultUri(TYPE_NOTIFICATION);
+            Ringtone r = getRingtone(context, notification);
+            r.play();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
