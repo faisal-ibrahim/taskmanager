02fca7b2bd40fd4d3cff81c8c91feeb821dc38cb
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2016-01-12 13:41:58 +0200

    Set title.

--- app/src/main/java/com/cretueusebiu/taskmanager/MainActivity.java
+++ app/src/main/java/com/cretueusebiu/taskmanager/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends AbstractActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Tasks");
 
         initialize();
 
